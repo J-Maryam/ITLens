8028a33704aec4a1631305d3c0bcd2fbf8cab8c9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-30 23:58:34 +0100

    Initial commit

---
+++ src/main/java/org/youcode/itlens/ItLensApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.itlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItLensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/youcode/itlens/ItLensApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.itlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
