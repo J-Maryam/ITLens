edbfea487b4777a5fd756ef912c0193833677bfa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 12:20:03 +0100

    update generic service

--- src/main/java/org/youcode/itlens/common/application/service/GenericService.java
+++ src/main/java/org/youcode/itlens/common/application/service/GenericService.java
@@ -3,9 +3,9 @@ package org.youcode.itlens.common.application.service;
 import java.util.List;
 
 public interface GenericService <T, ID, RequestDto, ResponseDto>{
-    List<T> getAll();
-    T getById(ID id);
-    T create(RequestDto requestDto);
-    T update(ID id, RequestDto requestDto);
+    List<ResponseDto> getAll();
+    ResponseDto getById(ID id);
+    ResponseDto create(RequestDto requestDto);
+    ResponseDto update(ID id, RequestDto requestDto);
     void delete(ID id);
 }
