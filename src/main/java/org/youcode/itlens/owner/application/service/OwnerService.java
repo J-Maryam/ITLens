d5745b786ede2223b45890210bd4ae1c9ad5969d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 12:11:35 +0100

    createi initial owner service

--- src/main/java/org/youcode/itlens/owner/application/service/OwnerService.java
+++ src/main/java/org/youcode/itlens/owner/application/service/OwnerService.java
@@ -1,4 +1,9 @@
 package org.youcode.itlens.owner.application.service;
 
-public interface OwnerService {
+import org.youcode.itlens.common.application.service.GenericService;
+import org.youcode.itlens.owner.application.dto.OwnerRequestDTO;
+import org.youcode.itlens.owner.application.dto.OwnerResponseDTO;
+import org.youcode.itlens.owner.domain.Owner;
+
+public interface OwnerService extends GenericService<Owner, Long, OwnerRequestDTO, OwnerResponseDTO> {
 }
--- src/main/java/org/youcode/itlens/owner/application/service/OwnerServiceImpl.java
+++ src/main/java/org/youcode/itlens/owner/application/service/OwnerServiceImpl.java
@@ -1,4 +1,47 @@
 package org.youcode.itlens.owner.application.service;
 
-public class OwnerServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.youcode.itlens.owner.application.dto.OwnerRequestDTO;
+import org.youcode.itlens.owner.application.mapper.OwnerMapper;
+import org.youcode.itlens.owner.domain.Owner;
+import org.youcode.itlens.owner.domain.OwnerRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+@Validated
+@RequiredArgsConstructor
+public class OwnerServiceImpl implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final OwnerMapper ownerMapper;
+
+    @Override
+    public List<Owner> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Owner getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Owner create(OwnerRequestDTO ownerRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public Owner update(Long aLong, OwnerRequestDTO ownerRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
 }
