28f4ed6724fffca6c0fc6642b3cea1625f901258
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 14:28:09 +0100

    feat: Add findAllOwners endpoint in OwnerController

--- src/main/java/org/youcode/itlens/owner/infrastructure/web/OwnerController.java
+++ src/main/java/org/youcode/itlens/owner/infrastructure/web/OwnerController.java
@@ -1,10 +1,14 @@
 package org.youcode.itlens.owner.infrastructure.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.youcode.itlens.owner.application.dto.OwnerResponseDTO;
 import org.youcode.itlens.owner.application.service.OwnerService;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/owners")
@@ -12,5 +16,9 @@ import org.youcode.itlens.owner.application.service.OwnerService;
 public class OwnerController {
     private final OwnerService service;
 
-
+    @GetMapping
+    public ResponseEntity<List<OwnerResponseDTO>> findAll() {
+        List<OwnerResponseDTO> owners = service.getAll();
+        return ResponseEntity.ok(owners);
+    }
 }
