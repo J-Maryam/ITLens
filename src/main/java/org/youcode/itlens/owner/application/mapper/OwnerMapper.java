acd31abe8ae984bb52ba3a7b373719f4379d6906
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 12:07:19 +0100

    create owner mapper

--- src/main/java/org/youcode/itlens/owner/application/mapper/OwnerMapper.java
+++ src/main/java/org/youcode/itlens/owner/application/mapper/OwnerMapper.java
@@ -1,4 +1,11 @@
 package org.youcode.itlens.owner.application.mapper;
 
-public class OwnerMapper {
+import org.mapstruct.Mapper;
+import org.youcode.itlens.common.application.mapper.GenericMapper;
+import org.youcode.itlens.owner.application.dto.OwnerRequestDTO;
+import org.youcode.itlens.owner.application.dto.OwnerResponseDTO;
+import org.youcode.itlens.owner.domain.Owner;
+
+@Mapper(componentModel = "spring")
+public interface OwnerMapper extends GenericMapper<Owner, OwnerRequestDTO, OwnerResponseDTO> {
 }
