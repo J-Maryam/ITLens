76ca7e7f79fa08d96284bfa47057aa2756c09bd3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 11:58:24 +0100

    create response dto for owner

--- src/main/java/org/youcode/itlens/owner/application/dto/OwnerResponseDTO.java
+++ src/main/java/org/youcode/itlens/owner/application/dto/OwnerResponseDTO.java
@@ -1,4 +1,12 @@
 package org.youcode.itlens.owner.application.dto;
+gi
+import org.youcode.itlens.survey.domain.entities.Survey;
 
-public record OwnerResponseDTO() {
+import java.util.List;
+
+public record OwnerResponseDTO(
+        Long id,
+        String name,
+        List<Survey>surveys
+) {
 }
