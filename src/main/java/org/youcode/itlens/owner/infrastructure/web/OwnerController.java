58a4eb45c7b086f021fa5bb503c26ba6b814a12f
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-05 13:44:22 +0100

    feat: Add OwnerController with getAllOwner endpoint

--- src/main/java/org/youcode/itlens/owner/infrastructure/web/OwnerController.java
+++ src/main/java/org/youcode/itlens/owner/infrastructure/web/OwnerController.java
@@ -1,4 +1,16 @@
 package org.youcode.itlens.owner.infrastructure.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.itlens.owner.application.service.OwnerService;
+
+
+@RestController
+@RequestMapping("api/v1/owners")
+@RequiredArgsConstructor
 public class OwnerController {
+    private final OwnerService service;
+
+
 }
