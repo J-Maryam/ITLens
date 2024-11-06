90ebee25ed47345c4933b86a73d5c0b6698ffe1c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-06 17:39:57 +0100

    feat: remove unused import in AnswerRequestDto

--- src/main/java/org/youcode/itlens/survey/application/dto/request/AnswerRequestDto.java
+++ src/main/java/org/youcode/itlens/survey/application/dto/request/AnswerRequestDto.java
@@ -1,13 +1,10 @@
 package org.youcode.itlens.survey.application.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record AnswerRequestDto(
-        @NotBlank @Size(min = 3, max = 255)String text,
+        String text,
 
-        @NotNull
-        Long questionId
+        @NotNull Long questionId
 ) {
 }
