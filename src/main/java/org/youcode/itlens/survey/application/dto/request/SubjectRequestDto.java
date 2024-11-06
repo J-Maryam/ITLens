f774073d407820057c7dad9969a0f4916b9075df
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-06 17:54:44 +0100

    feat: add @Exists annotation for surveyEditionId and parentSubjectId in the SubjectRequestDto

--- src/main/java/org/youcode/itlens/survey/application/dto/request/SubjectRequestDto.java
+++ src/main/java/org/youcode/itlens/survey/application/dto/request/SubjectRequestDto.java
@@ -3,17 +3,20 @@ package org.youcode.itlens.survey.application.dto.request;
 import jakarta.persistence.Column;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.youcode.itlens.common.application.validation.annotation.Exists;
 import org.youcode.itlens.survey.domain.entities.Subject;
 import org.youcode.itlens.survey.domain.entities.SurveyEdition;
 
 public record SubjectRequestDto(
-        @NotBlank
-        String title,
+        @NotBlank String title,
 
-        @ManyToOne
-        SurveyEdition surveyEdition,
+        @NotNull
+        @Exists(entityClass = SurveyEdition.class, message = "no survey edition with this id")
+        Long surveyEditionId,
 
-        @ManyToOne
-        Subject parentSubject
+        @NotNull
+        @Exists(entityClass = Subject.class, message = "no parent subject with this id")
+        Long parentSubjectId
 ) {
 }
