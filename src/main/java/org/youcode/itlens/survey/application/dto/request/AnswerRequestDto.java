2648e3f825b2a9f31c9853c37868b89d493dc332
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-06 18:09:57 +0100

    add annotations in the AnswerRequestDto

--- src/main/java/org/youcode/itlens/survey/application/dto/request/AnswerRequestDto.java
+++ src/main/java/org/youcode/itlens/survey/application/dto/request/AnswerRequestDto.java
@@ -1,10 +1,13 @@
 package org.youcode.itlens.survey.application.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import org.youcode.itlens.common.application.validation.annotation.Exists;
+import org.youcode.itlens.survey.domain.entities.Question;
 
 public record AnswerRequestDto(
         String text,
 
+        @Exists(entityClass = Question.class, message = "Question id does not exists")
         @NotNull Long questionId
 ) {
 }
