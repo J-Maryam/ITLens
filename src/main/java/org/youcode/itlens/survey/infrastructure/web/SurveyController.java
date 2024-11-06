fef84ad4cc89c0df6c248c041d7ee532a7d0a2eb
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-06 22:52:35 +0100

    feat: Add SurveyController with findAll endpoint

---
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/SurveyController.java
@@ -0,0 +1,25 @@
+package org.youcode.itlens.survey.infrastructure.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.youcode.itlens.owner.application.dto.OwnerResponseDTO;
+import org.youcode.itlens.survey.application.dto.response.SurveyResponseDto;
+import org.youcode.itlens.survey.application.service.SurveyService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/surveys")
+public class SurveyController {
+    private final SurveyService service;
+
+    @GetMapping
+    public ResponseEntity<List<SurveyResponseDto>> findAll() {
+        List<SurveyResponseDto> surveys = service.getAll();
+        return ResponseEntity.ok(surveys);
+    }
+}
