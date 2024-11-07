e59d8eaeb1590d79f1932fb4a3446c2660e45f99
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-07 18:55:58 +0100

    feat: Add findAllBySurveyEditionId method in the SubjectRepository

--- src/main/java/org/youcode/itlens/survey/domain/repository/SubjectRepository.java
+++ src/main/java/org/youcode/itlens/survey/domain/repository/SubjectRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.youcode.itlens.survey.domain.entities.Subject;
 import org.youcode.itlens.survey.domain.entities.SurveyEdition;
 
+import java.util.List;
+
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     boolean existsByTitleAndSurveyEdition(String title, SurveyEdition surveyEdition);
-
+    List<Subject> findAllBySurveyEditionId(Long surveyEditionId);
 }
