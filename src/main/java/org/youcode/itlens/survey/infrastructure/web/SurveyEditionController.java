392514c3affac902d4c1f3b629d8a678b69b5e2a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-07 12:33:38 +0100

    feat: Add getAll endpoint in the SurveyEditionController

--- src/main/java/org/youcode/itlens/survey/infrastructure/web/SurveyEditionController.java
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/SurveyEditionController.java
@@ -1,13 +1,24 @@
 package org.youcode.itlens.survey.infrastructure.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.youcode.itlens.survey.application.dto.response.SurveyEditionResponseDto;
 import org.youcode.itlens.survey.application.service.SurveyEditionService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/survey-editions")
 public class SurveyEditionController {
     private final SurveyEditionService service;
+
+    @GetMapping
+    public ResponseEntity<List<SurveyEditionResponseDto>> findAll() {
+        List<SurveyEditionResponseDto> surveyEditions = service.getAll();
+        return ResponseEntity.ok(surveyEditions);
+    }
 }
