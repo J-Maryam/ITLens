9e9fd446d72f63d4770430b4a134c0c0946e544a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-07 22:17:21 +0100

    feat: Add getAll endpoint in the SubjectController

--- src/main/java/org/youcode/itlens/survey/infrastructure/web/SubjectController.java
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/SubjectController.java
@@ -4,15 +4,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.itlens.survey.application.dto.request.SubjectRequestDto;
 import org.youcode.itlens.survey.application.dto.request.SurveyRequestDto;
 import org.youcode.itlens.survey.application.dto.response.SubjectResponseDto;
 import org.youcode.itlens.survey.application.dto.response.SurveyResponseDto;
 import org.youcode.itlens.survey.application.service.SubjectService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,11 @@ public class SubjectController {
     public ResponseEntity<SubjectResponseDto> create(@RequestBody @Valid SubjectRequestDto dto) {
         SubjectResponseDto newSubject = service.create(dto);
         return new ResponseEntity<>(newSubject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/subjects")
+    public ResponseEntity<List<SubjectResponseDto>> findAll() {
+        List<SubjectResponseDto> surveys = service.getAll();
+        return ResponseEntity.ok(surveys);
     }
 }
