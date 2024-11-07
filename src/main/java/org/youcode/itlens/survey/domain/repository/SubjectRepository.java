9d2d6df11c81880c8dac2fcbb299c37479aa084c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-07 18:28:14 +0100

    feat: Add existTitleAndSurveyEdition method in the SubjectRepository

--- src/main/java/org/youcode/itlens/survey/domain/repository/SubjectRepository.java
+++ src/main/java/org/youcode/itlens/survey/domain/repository/SubjectRepository.java
@@ -2,6 +2,9 @@ package org.youcode.itlens.survey.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.youcode.itlens.survey.domain.entities.Subject;
+import org.youcode.itlens.survey.domain.entities.SurveyEdition;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    boolean existsByTitleAndSurveyEdition(String title, SurveyEdition surveyEdition);
+
 }
