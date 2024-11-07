29b1ec3b6fece10c11ed9034623975973c6e1867
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-07 12:36:24 +0100

    feat: Add getAll endpoint in the SurveyEditionController

--- src/main/java/org/youcode/itlens/ItLensApplication.java
+++ src/main/java/org/youcode/itlens/ItLensApplication.java
@@ -3,7 +3,8 @@ package org.youcode.itlens;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.youcode.itlens")
 public class ItLensApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/youcode/itlens/survey/infrastructure/web/SurveyEditionController.java
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/SurveyEditionController.java
@@ -3,9 +3,11 @@ package org.youcode.itlens.survey.infrastructure.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.youcode.itlens.survey.application.dto.response.SurveyEditionResponseDto;
+import org.youcode.itlens.survey.application.dto.response.SurveyResponseDto;
 import org.youcode.itlens.survey.application.service.SurveyEditionService;
 
 import java.util.List;
@@ -20,5 +22,11 @@ public class SurveyEditionController {
     public ResponseEntity<List<SurveyEditionResponseDto>> findAll() {
         List<SurveyEditionResponseDto> surveyEditions = service.getAll();
         return ResponseEntity.ok(surveyEditions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SurveyEditionResponseDto> findById(@PathVariable Long id) {
+        SurveyEditionResponseDto surveyEdition = service.getById(id);
+        return ResponseEntity.ok(surveyEdition);
     }
 }
