2f1ac8725019b349570dde76a2ab7d897cb1598c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-08 16:29:16 +0100

    feat: Add create answer method int the AnswerServiceImpl

--- src/main/java/org/youcode/itlens/survey/application/service/Impl/AnswerServiceImpl.java
+++ src/main/java/org/youcode/itlens/survey/application/service/Impl/AnswerServiceImpl.java
@@ -3,18 +3,27 @@ package org.youcode.itlens.survey.application.service.Impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.youcode.itlens.common.domain.exception.EntityNotFoundException;
 import org.youcode.itlens.survey.application.dto.request.AnswerRequestDto;
 import org.youcode.itlens.survey.application.dto.response.AnswerResponseDto;
+import org.youcode.itlens.survey.application.mapper.AnswerMapper;
 import org.youcode.itlens.survey.application.service.AnswerService;
+import org.youcode.itlens.survey.domain.entities.Answer;
+import org.youcode.itlens.survey.domain.entities.Question;
 import org.youcode.itlens.survey.domain.repository.AnswerRepository;
+import org.youcode.itlens.survey.domain.repository.QuestionRepository;
 
 import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Validated
 public class AnswerServiceImpl implements AnswerService {
     private final AnswerRepository repository;
+    private final QuestionRepository questionRepository;
+    private final AnswerMapper mapper;
 
     @Override
     public List<AnswerResponseDto> getAll() {
@@ -27,8 +36,14 @@ public class AnswerServiceImpl implements AnswerService {
     }
 
     @Override
-    public AnswerResponseDto create(AnswerRequestDto answerRequestDto) {
-        return null;
+    public AnswerResponseDto create(AnswerRequestDto requestDto) {
+        Question question = questionRepository.findById(requestDto.questionId())
+                .orElseThrow(() -> new EntityNotFoundException("Question with Id " + requestDto.questionId() + " not found"));
+
+        Answer answer = mapper.toEntity(requestDto);
+        answer.setQuestion(question);
+        answer = repository.save(answer);
+        return mapper.toDto(answer);
     }
 
     @Override
