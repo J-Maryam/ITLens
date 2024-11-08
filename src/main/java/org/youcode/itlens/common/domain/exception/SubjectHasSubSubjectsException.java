2a74f99cd9b88b5c9e540d43a0bbcc2ee5c0c5a8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-08 01:04:17 +0100

    feat: Add SubjectHasSubSubjectsException exception

---
+++ src/main/java/org/youcode/itlens/common/domain/exception/SubjectHasSubSubjectsException.java
@@ -0,0 +1,7 @@
+package org.youcode.itlens.common.domain.exception;
+
+public class SubjectHasSubSubjectsException extends RuntimeException {
+    public SubjectHasSubSubjectsException(String message) {
+        super(message);
+    }
+}
