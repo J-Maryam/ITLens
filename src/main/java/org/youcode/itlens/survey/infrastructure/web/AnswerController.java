0de17be886e818c576252880f1b62995309d4a68
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-08 16:46:52 +0100

    feat: Add getAll endpoint in AnswerController

--- src/main/java/org/youcode/itlens/survey/infrastructure/web/AnswerController.java
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/AnswerController.java
@@ -3,6 +3,7 @@ package org.youcode.itlens.survey.infrastructure.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,19 @@ import org.youcode.itlens.survey.application.dto.request.AnswerRequestDto;
 import org.youcode.itlens.survey.application.dto.response.AnswerResponseDto;
 import org.youcode.itlens.survey.application.service.AnswerService;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/answers")
 public class AnswerController {
     private final AnswerService service;
+
+    @GetMapping
+    public ResponseEntity<List<AnswerResponseDto>> getAll() {
+        List<AnswerResponseDto> answers = service.getAll();
+        return ResponseEntity.ok(answers);
+    }
 
     @PostMapping
     public ResponseEntity<AnswerResponseDto> create(AnswerRequestDto requestDto) {
