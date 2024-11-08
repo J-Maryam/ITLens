2502bf542aa1d2a146c544e5a4e44cfa31956274
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-08 16:48:56 +0100

    feat: Add getById endpoint in AnswerController

--- src/main/java/org/youcode/itlens/survey/infrastructure/web/AnswerController.java
+++ src/main/java/org/youcode/itlens/survey/infrastructure/web/AnswerController.java
@@ -3,10 +3,7 @@ package org.youcode.itlens.survey.infrastructure.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.itlens.survey.application.dto.request.AnswerRequestDto;
 import org.youcode.itlens.survey.application.dto.response.AnswerResponseDto;
 import org.youcode.itlens.survey.application.service.AnswerService;
@@ -23,6 +20,12 @@ public class AnswerController {
     public ResponseEntity<List<AnswerResponseDto>> getAll() {
         List<AnswerResponseDto> answers = service.getAll();
         return ResponseEntity.ok(answers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AnswerResponseDto> getById(@PathVariable Long id) {
+        AnswerResponseDto answer = service.getById(id);
+        return ResponseEntity.ok(answer);
     }
 
     @PostMapping
