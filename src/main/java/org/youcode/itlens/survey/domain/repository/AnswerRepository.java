53afd254b4d47fa7c34505c626b19f565e7e1029
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-10 12:56:13 +0100

    feat: Add findAllByIdInRange in AnwserRepository

--- src/main/java/org/youcode/itlens/survey/domain/repository/AnswerRepository.java
+++ src/main/java/org/youcode/itlens/survey/domain/repository/AnswerRepository.java
@@ -1,9 +1,15 @@
 package org.youcode.itlens.survey.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.youcode.itlens.survey.domain.entities.Answer;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    @Query("SELECT a FROM Answer a WHERE a.id BETWEEN :startId AND :endId")
+    List<Answer> findAllByIdInRange(@Param("startId") Long startId, @Param("endId") Long endId);
 }
